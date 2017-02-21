08b7755eff541e39deeb78f633b44a86d3263699
JBP1996 <jbp.1996@hotmail.com>
2017-02-21 14:20:00 +0000

    adicionar ficheiros visio

---
+++ aula1/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aula1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownList1.Items.Add(new ListItem("Terceira Opção", "3"));
+            }
+            else
+            {
+
+            }
+
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Nome.Text = "Hello World";
+        }
+    }
+}
